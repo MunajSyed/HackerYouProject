79b41f9c95dfb84d8be3e692f99d73f8389c3550
Michael Perrotte <michael.perrotte@resolver.com>
2019-03-24 18:02:18 -0400

    feat(day4,example): updated stockService with createStock method

--- code/api/routes/stocks/stockService.js
+++ code/api/routes/stocks/stockService.js
@@ -6,4 +6,12 @@ const PortfolioModel = require('../portfolios/portfolioModel');
 exports.listStocks = async () => await StockModel.find();
 
 exports.createStock = async (stockData) => {
+  const stock = new StockModel(stockData);
+  try {
+    const doc = await stock.save();
+    return doc;
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
 };
